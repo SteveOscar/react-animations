83b0c104fd3796187aab9c4df983573e03f0acd3
Steve Olson <steve.olson@rxrevu.com>
2018-12-26 09:25:12 -0700

    sets up background color toggling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import First from './First.js'
 import Trails from './Trails.js'
 import Toggler from './Toggler.js'
+import Flipper from './Flipper.js'
 import TrailsSecond from './TrailsSecond.js'
 import logo from './logo.svg';
 import './App.css';
@@ -9,16 +10,39 @@ import './index.css';
 
 import { Spring, Transition, Trail, config } from 'react-spring'
 
+const styles = {
+  background: { height: '100%', width: '100%', position: 'absolute' },
+}
+const Background = ({ backgroundColor }) => (
+  // <div style={{ ...styles.container, background: `linear-gradient(to bottom, ${start} ${stop}, ${end} 100%)` }}>
+  <div style={{ ...styles.background, backgroundColor: backgroundColor }}>
+  </div>
+)
+
 class App extends Component {
+  state = { toggle: true }
+  
+  toggle = () => this.setState(state => ({ toggle: !state.toggle }))
+
+  clickDetected = () => {
+    this.toggle()
+    console.log('CLICK!')
+  }
   render() {
+    const toggle = this.state.toggle
     return (
       <div className="App">
         <header className="App-header">
-          <Toggler />
-          'Experience'
+          <Spring
+            from={{ backgroundColor: 'yellow' }}
+            to={{ backgroundColor: toggle ? 'blue' : 'red' }}
+            children={Background}
+          />
+          <Toggler clickDetected={this.clickDetected} />
+          Experience
           <br/>
           <br/>
-          <Toggler />
+          <Flipper clickDetected={this.clickDetected} />
         </header>
       </div>
     );
--- src/Toggler.js
+++ src/Toggler.js
@@ -5,15 +5,15 @@ const TRIANGLE = 'M20,380 L380,380 L380,380 L200,20 L20,380 Z'
 const RECTANGLE = 'M20,20 L20,380 L380,380 L380,20 L20,20 Z'
 const CIRCLE = 'M20,20 L20,380 L380,380 L380,20 L20,20 Z'
 const styles = {
-  container: { height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', willChange: 'background' },
+  container: { marginBottom: '-90px', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', willChange: 'background' },
   shape: { width: 300, height: 300, willChange: 'transform' }
 }
 
-const Content = ({ toggle, color, scale, shape, start, end, stop, rotation, text, opacity }) => (
+const Content = ({ toggle, color, scale, shape, start, end, stop, rotation, opacity }) => (
   // <div style={{ ...styles.container, background: `linear-gradient(to bottom, ${start} ${stop}, ${end} 100%)` }}>
   <div style={{ ...styles.container, background: `transparent` }}>
     <svg
-      style={{ ...styles.shape, transform: `scale3d(${scale}, ${scale}, ${scale}) rotate(${rotation})`, borderStyle: 'solid', borderWidth: '2px' }}
+      style={{ ...styles.shape, transform: `scale3d(${scale}, ${scale}, ${scale}) rotate(${rotation})` }}
       version="1.1"
       viewBox="0 0 400 400">
       <g style={{ cursor: 'pointer' }} fill={color} fillRule="evenodd" onClick={toggle}>
@@ -21,31 +21,32 @@ const Content = ({ toggle, color, scale, shape, start, end, stop, rotation, text
       </g>
     </svg>
     <span style={{position: 'absolute', color: '#282c34', opacity: opacity}}>
-      {text}
+      Scarlett <br/> Merry Christmas!!
+      
     </span>
   </div>
 )
 // export default class Toggler extends React.PureComponent {
 class Toggler extends React.Component {
   state = { toggle: true }
-  toggle = () => this.setState(state => ({ toggle: !state.toggle }))
+  toggle = () => {
+    this.props.clickDetected()
+    this.setState(state => ({ toggle: !state.toggle }))
+  }
   render() {
     const toggle = this.state.toggle
-    const experience = 'stuff/more stuff'
-    const text = toggle ? '' : experience
     return (
       <div onClick={this.toggle}>
       <Spring
-        from={{ color: 'black' }}
+        from={{ color: 'white' }}
         to={{
           color: toggle ? '#247BA0' : '#70C1B3',
           start: toggle ? '#B2DBBF' : '#B2DBBF',
           end: toggle ? '#247BA0' : '#F3FFBD',
-          scale: toggle ? 0.6 : 1.5,
+          scale: toggle ? 0.2 : 1.5,
           shape: toggle ? CIRCLE : RECTANGLE,
           stop: toggle ? '0%' : '50%',
           rotation: toggle ? '0deg' : '45deg',
-          text: text,
           opacity: toggle ? 0 : 1
         }}
         toggle={this.toggle} // Additional props will be spread over the child
